5c6bc7bc55b437a88bd93614f8876eafa0a78668
andreataglia <drew.taglia@gmail.com>
2018-10-08 16:13:37 +0800

    fixed empty while

--- jnodes/JWhileStmt.java
+++ jnodes/JWhileStmt.java
@@ -3,17 +3,15 @@ package jnodes;
 //WHILE LPAREN exp RPAREN LBRACE stmt stmtList RBRACE
 public class JWhileStmt extends JStmt {
     public JExp exp;
-    public JStmt stmt;
     public JStmtList stmtList;
 
-    public JWhileStmt(JExp exp, JStmt stmt, JStmtList stmtList) {
+    public JWhileStmt(JExp exp, JStmtList stmtList) {
         this.exp = exp;
-        this.stmt = stmt;
         this.stmtList = stmtList;
     }
 
     @Override
     public String toString() {
-        return "while(" + exp + "){\n" + stmt + " " + stmtList + "\n}\n";
+        return "while(" + exp + "){\n" + stmtList + "\n}\n";
     }
 }
